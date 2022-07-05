011abae05d7bc7f89dd4bb255eff7024480674a5
davegreensmith <dave_greensmith@msn.com>
2022-07-05 14:54:55 +0100

    remove file included in gitignore

--- db/test.sql
+++
@@ -1,7 +0,0 @@
-\c nc_games_test
-
-SELECT reviews.*, COUNT(comments.comment_id) AS comment_count FROM reviews
-JOIN comments ON reviews.review_id = comments.review_id
-WHERE reviews.review_id=3
-GROUP BY reviews.review_id;
-
