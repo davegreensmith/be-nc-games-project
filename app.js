70cccdd3f58a6d88d8cb57cf8db261d2647e1788
davegreensmith <dave_greensmith@msn.com>
2022-07-05 16:58:02 +0100

    merge changes

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
-const { getReviewById, getCategories, patchReviewById, getUsers, getCommentsByReviewId } = require('./controllers/controller.games');
-=======
-const { getReviewById, getCategories, patchReviewById, getUsers, getReviews } = require('./controllers/controller.games');
->>>>>>> main
+
+const { getReviewById, getCategories, patchReviewById, getUsers, getReviews, getCommentsByReviewId } = require('./controllers/controller.games');
+
 const { handleCustomErrors, handlePSQLErrors, unhandledErrors, noPath } = require('./errors/error-handlers.js');
 
 app.use(express.json());
@@ -13,11 +11,8 @@ app.get('/api/categories', getCategories);
 app.get('/api/categories', getCategories);
 app.get('/api/review/:review_id', getReviewById);
 app.get('/api/users', getUsers);
-<<<<<<< HEAD
 app.get('/api/reviews/:review_id/comments', getCommentsByReviewId);
-=======
 app.get('/api/reviews', getReviews);
->>>>>>> main
 
 app.patch('/api/reviews/:review_id', patchReviewById);
 
