33481dc9fcf60cc799668f3b4a8001978ec36058
davegreensmith <dave_greensmith@msn.com>
2022-07-08 10:40:42 +0100

    refactor DELETE api on comments

--- models/models.comments.js
+++ models/models.comments.js
@@ -38,11 +38,9 @@ exports.checkUserExists = (username) => {
 };
 
 exports.removeCommentByCommentId = (comment_id) => {
-  return Promise.all([db.query('DELETE FROM comments WHERE comment_id = $1 RETURNING *', [comment_id]), comment_id]).then(([{ rows }, comment_id]) => {
-    return Promise.all([db.query('SELECT * FROM comments WHERE comment_id=$1', [comment_id]), rows]).then(([{ rows }, deleteResult]) => {
-      if (rows.length === 0 && deleteResult.length === 0) {
-        return Promise.reject({ status: 404, msg: 'Comment not found' });
-      }
-    });
+  return db.query('DELETE FROM comments WHERE comment_id = $1 RETURNING *', [comment_id]).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: 'Comment not found' });
+    }
   });
 };
