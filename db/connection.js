72c2c11c8c5df431e3c51721d5a1570e203c6fd6
davegreensmith <dave_greensmith@msn.com>
2022-07-08 14:20:22 +0100

    modifications for Heroku

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,7 @@ require('dotenv').config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
-
+console.log(ENV);
 const config =
   ENV === 'production'
     ? {
